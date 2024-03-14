fed0e395a189bd52df0cbd40bf83862bd938e2a4
LudvigEk <ludvig.ekdahl@med.lu.se>
2024-03-14 22:34:00 +0100

    favorites functionality

--- source/main.py
+++ source/main.py
@@ -3,9 +3,11 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from json import load
+from json import load, dump
 from kivy.clock import Clock
 from functools import partial # allow arguments in Clock.schedule_once
+import webbrowser # to allow hyperlinks in the help screen
+
 """
 Author Ludvig Ekdahl, 2024 
 PathPoints is a Kivy app for keeping track of weighted points in a cyto- and/or 
@@ -30,6 +32,37 @@ def get_subject_list(subject_name):
         subjects_json = load(file)
         return subjects_json["subjects"]
 
+def add_favorite(Button, subject_name, points, json_file, *args, **kwargs):
+    # Add a subject to the diagnostic favorites json
+    with open(json_file, "r") as file:
+        favorites_json = load(file)
+        # check if the subject is already in the favorites
+        if subject_name in favorites_json["samples"].keys():
+            file.close()
+            return
+        favorites_json["samples"][subject_name] = points
+    file.close()
+    with open(json_file, "w") as file:
+        # write json
+        dump(favorites_json, file)
+    file.close()
+
+def remove_favorite(Button, subject_name, json_file, *args, **kwargs):
+    # Remove a subject from the diagnostic favorites json
+    with open(json_file, "r") as file:
+        favorites_json = load(file)
+        # check if the subject is already in the favorites
+        if subject_name not in favorites_json["samples"].keys():
+            file.close()
+            return
+        del favorites_json["samples"][subject_name]
+    file.close()
+    with open(json_file, "w") as file:
+        # write json
+        dump(favorites_json, file)
+    file.close()
+
+
 class WindowManager(ScreenManager):
     pass
 
@@ -51,11 +84,12 @@ class PathPointsMain(Screen):
         super().__init__(**kwargs)
 
         # Have to delay the initialization of the subject screens to until PathPointsMain is fully loaded
-        Clock.schedule_once(partial(self.initialize_subscreens, "all_diagnostic_subjects", "subject_lists/diagnostics_subjects.json"))
-        Clock.schedule_once(partial(self.initialize_subscreens, "all_grossing_subjects", "subject_lists/grossing_subjects.json"))
-        # ToDO set up favorites
+        Clock.schedule_once(partial(self.initialize_subscreens, "all_diagnostic_subjects", "subject_lists/diagnostics_subjects.json", True))
+        Clock.schedule_once(partial(self.initialize_subscreens, "all_grossing_subjects", "subject_lists/grossing_subjects.json", False))
+        Clock.schedule_once(partial(self.refresh_favorite, "favorite_grossing_subjects", "favorites/favorites-grossing.json", False))
+        Clock.schedule_once(partial(self.refresh_favorite, "favorite_diagnostic_subjects", "favorites/favorites-diagnostics.json", True))
 
-    def initialize_subscreens(self, screen_name, subject_file, *args):
+    def initialize_subscreens(self, screen_name, subject_file, diagnostic, *args):
         # Collect the all_subjects_screen handle
         all_subjects_screen = self.manager.get_screen(screen_name)
         # Collect the all_subjects_grid element from the all_subjects_screen handle
@@ -64,6 +98,7 @@ class PathPointsMain(Screen):
         with open(subject_file, "r") as file:
             subjects_json = load(file)
             subjects_dict = subjects_json["subjects"]
+        file.close()
         # Initialize rows in all_subjects_grid to number of keys in subjects
         all_subjects_grid.rows = len(subjects_dict)
         # Initialize columns in all_subjects_grid to 1
@@ -74,11 +109,34 @@ class PathPointsMain(Screen):
             # Create a button with the text of the subject
             subject_button = Button(text=subject)
             # Bind the view_subject function to the button as a callback
-            subject_button.bind(on_press=lambda x: self.view_subject(subject_file))
+            subject_button.bind(on_press=lambda x: self.view_subject(subject_file, diagnostic))
             # Add the button to the all_subjects_grid
             all_subjects_grid.add_widget(subject_button)
 
-    def view_subject(self, subject_name):
+    def refresh_favorite(self, screen_name, json_file, diagnostic, *args):
+        favorite_screen = self.manager.get_screen(screen_name)
+        favorite_grid = favorite_screen.ids.favorite_subjects_grid
+        favorite_grid.clear_widgets()
+        with open(json_file, "r") as file:
+            favorites_json = load(file)
+            favorites_dict = favorites_json["samples"]
+        file.close()
+        favorite_grid.rows = len(favorites_dict)
+        favorite_grid.cols = 4
+        for sample, points in favorites_dict.items():
+            # 0.05 padding between buttons
+            favorite_grid.add_widget(Label(text=sample, size_hint=(0.5, 0.1)))
+            favorite_grid.add_widget(Button(text="+", size_hint=(0.1, 0.1)))
+            favorite_grid.add_widget(Button(text="-", size_hint=(0.1, 0.1)))
+            subject_button = Button(text="Ta bort", size_hint=(0.1, 0.1))
+            if diagnostic:
+                subject_button.bind(on_press=partial(remove_favorite, subject_name=sample, json_file="favorites/favorites-diagnostics.json"))
+            else:
+                subject_button.bind(on_press=partial(remove_favorite, subject_name=sample, json_file="favorites/favorites-grossing.json"))
+            favorite_grid.add_widget(subject_button)
+
+
+    def view_subject(self, subject_name, diagnostic=True):
         """
         This function is called when a subject button is pressed. Upon clicking, the following happens:
         #1 It loads the json file for the subject into a dictionary
@@ -106,22 +164,20 @@ class PathPointsMain(Screen):
         # For each key in subject_dict, add a button to the subject_grid
         # after each such button, add a plus button, a minus button, and a favorite button
         # like this: <sample name> <plus> <minus> <favorite>
-        for key, value in subject_dict.items():
+        for sample, points in subject_dict.items():
             # 0.05 padding between buttons
-            subject_grid.add_widget(Label(text=key, size_hint=(0.5, 0.1)))
+            subject_grid.add_widget(Label(text=sample, size_hint=(0.5, 0.1)))
             subject_grid.add_widget(Button(text="+", size_hint=(0.1, 0.1)))
             subject_grid.add_widget(Button(text="-", size_hint=(0.1, 0.1)))
-            subject_grid.add_widget(Button(text="Add favorite", size_hint=(0.5, 0.1)))
+
+            subject_button = Button(text="Favorit", size_hint=(0.1, 0.1))
+            if diagnostic:
+                subject_button.fbind('on_press', add_favorite, subject_name=sample, points=points, json_file="favorites/favorites-diagnostics.json")
+            else:
+                subject_button.fbind('on_press', add_favorite, subject_name=sample, points=points, json_file="favorites/favorites-grossing.json")
+            subject_grid.add_widget(subject_button)
         # Change the user view to the SubjectScreen
         self.manager.current = "subject"
-
-    pass
-
-    def generate_report(self):
-        # Generate a text report (preferably pdf though)
-        # With the users points and statistics
-        pass
-
 
 class AllDiagnosticSubjectsScreen(Screen):
     pass
@@ -130,26 +186,17 @@ class AllGrossingSubjectsScreen(Screen):
     pass
 
 class SubjectScreen(Screen):
-    # Screen for showing buttons for a specific subject,
-    # Each defined in a .json
-    def add_favorite(self):
-        # Check if button name is in favorites json, otherwise add it
-        pass
-
     pass
 
-#help screen
 class HelpScreen(Screen):
     pass
 
-class FavoritesScreen(Screen):
+class FavoriteDiagnosticSubjectsScreen(Screen):
     # Screen for showing buttons for user-defined favorites
     # defined in a locally stored .json
+    pass
 
-    def remove_favorite(self):
-        # favorites screen cannot add favorites, just remove them
-        pass
-
+class FavoriteGrossingSubjectsScreen(Screen):
     pass
 
 kv = Builder.load_file("pathpoints.kv")
